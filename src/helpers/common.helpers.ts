0c4a67e52901f88c62433508f7c8192980bb8c57
Your User name <khizar621@loobr.io>
2023-10-16 20:21:59 +0500

    REpost run-time

--- src/helpers/common.helpers.ts
+++ src/helpers/common.helpers.ts
@@ -143,10 +143,12 @@ export function convertHashesMentionsToSimpleText(text) {
     const userMentionRegex = /\@\[([^\]]+)\]\(\w+\)/g;
     const linkRegex = /\$\[([^\]]+)\]\(([^)]+)\)/g;
 
-    const convertedText = text ? text
-        .replace(hashtagRegex, '#$1')
-        .replace(userMentionRegex, '@$1')
-        .replace(linkRegex, '$1') : text
+    const convertedText = text
+        ? text
+              .replace(hashtagRegex, '#$1')
+              .replace(userMentionRegex, '@$1')
+              .replace(linkRegex, '$1')
+        : text;
 
     return convertedText;
 }
