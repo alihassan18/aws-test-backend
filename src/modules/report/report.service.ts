2a86a817e86df3ff4acde9808e0ea6c4b0a4726e
Your User name <khizar621@loobr.io>
2023-10-16 15:28:06 +0500

    report issue

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -15,7 +15,7 @@ export class ReportService {
     constructor(
         @InjectModel(Report.name) private reportModal: Model<ReportDocument>,
         private readonly notificationService: NotificationService
-    ) {}
+    ) { }
 
     async create(
         data: CreateReportDto,
@@ -58,7 +58,7 @@ export class ReportService {
         if (data?.nft) {
             const alreadyReported = await this.reportModal.findOne({
                 // nft: new Types.ObjectId(data?.nft),
-                nft: data?.nft,
+                nft: { ...data.nft, contract: data.nft.contract?.toLowerCase() },
                 reportedBy: reportBy
             });
 
@@ -84,7 +84,7 @@ export class ReportService {
             //     reportedBy: reportBy
             // });
             const alreadyReported = await this.reportModal.findOne({
-                _collection: data?._collection,
+                _collection: { ...data._collection, contract: data._collection.contract?.toLowerCase() },
                 reportedBy: reportBy
             });
 
@@ -100,13 +100,13 @@ export class ReportService {
             ...(data.post && { post: new Types.ObjectId(data.post) }),
             ...(data.user && { user: new Types.ObjectId(data.user) }),
             // ...(data.nft && { nft: new Types.ObjectId(data.nft) }),
-            ...(data.nft && { nft: data.nft }),
+            ...(data.nft && { nft: { ...data.nft, contract: data.nft.contract?.toLowerCase() } }),
             ...(data.land && { land: new Types.ObjectId(data.land) }),
             // ...(data._collection && {
             //     _collection: new Types.ObjectId(data._collection)
             // }),
             ...(data._collection && {
-                _collection: data._collection
+                _collection: { ...data._collection, contract: data._collection.contract?.toLowerCase() }
             }),
             reportedBy: reportBy
         });
@@ -205,7 +205,7 @@ export class ReportService {
         const isAvailable = await this.reportModal.findOne({
             type: ReportStatus.COMPLETED,
             $and: [
-                { 'nft.contract': contract },
+                { 'nft.contract': contract?.toLowerCase() },
                 { 'nft.chain': chain },
                 { 'nft.tokenId': tokenId },
                 { 'nft.isBlocked': true }
@@ -223,14 +223,14 @@ export class ReportService {
             type: ReportStatus.COMPLETED,
             $and: [
                 {
-                    '_collection.contract': {
-                        $regex: new RegExp(`${contract}`, 'i')
-                    }
+                    '_collection.contract': contract?.toLowerCase()
                 },
                 { '_collection.chain': chain },
                 { '_collection.isBlocked': true }
             ]
         });
+        console.log(isAvailable, 'isAvailable');
+
         if (isAvailable) {
             return { success: true };
         } else {
