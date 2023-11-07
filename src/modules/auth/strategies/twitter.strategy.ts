7b2d0fde6e92772f5a870313834c8e256b431619
Ishan Shahzad <3006761319ishan@gmail.com>
2023-11-07 19:24:00 +0500

    twitter initilize

--- src/modules/auth/strategies/twitter.strategy.ts
+++ src/modules/auth/strategies/twitter.strategy.ts
@@ -8,10 +8,7 @@ import { Model } from 'mongoose';
 import { Strategy, Profile } from 'passport-twitter';
 import { USERS } from 'src/constants/db.collections';
 import { jwtConstants } from 'src/constants/jwt.constant';
-import {
-    Referral,
-    ReferralDocument
-} from 'src/modules/referral/entities/referral.entity';
+import { Referral, ReferralDocument } from 'src/modules/referral/entities/referral.entity';
 import { UserDocument } from 'src/modules/users/entities/user.entity';
 
 interface Info {
@@ -24,11 +21,11 @@ export class TwitterStrategy extends PassportStrategy(Strategy) {
         @InjectModel(USERS) readonly userModel: Model<UserDocument>,
         @InjectModel(Referral.name)
         readonly referralModel: Model<ReferralDocument>,
-        private readonly jwtService: JwtService
+        private readonly jwtService: JwtService,
     ) {
         super({
-            consumerKey: process.env.TWITTER_CONSUMER_KEY,
-            consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+            consumerKey: "RdKeJZgn21yOyP5aKt8piFvWg",
+            consumerSecret:"IZhfVaIT9PfeRMXo8Nv8L4ZociO3g0UuFimYn4MaoZlv19ZT2F",
             callbackURL: `${process.env.BASE_URL}/auth/twitter/callback`,
             includeEmail: true,
             passReqToCallback: true
@@ -235,7 +232,7 @@ export class TwitterStrategy extends PassportStrategy(Strategy) {
                 });
             }
         } catch (error) {
-            console.log(error, 'error-=');
+            console.log(error,'error-=')
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
             done(error);
