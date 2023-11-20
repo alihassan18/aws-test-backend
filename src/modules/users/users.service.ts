3ee06285e999bdcb243673add8e4dc5075b51bea
Ishan Shahzad <3006761319ishan@gmail.com>
2023-11-20 18:48:49 +0500

    report 42

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1088,7 +1088,10 @@ export class UsersService {
                         scc_status: 'PENDING'
                     }
                 });
-                return { message: translate("messages.applied"), success: true };
+                return {
+                    message: translate('messages.applied'),
+                    success: true
+                };
             } else {
                 return null;
             }
@@ -1119,7 +1122,10 @@ export class UsersService {
                     message: 'You have become a Content Creater Badge!',
                     receiver: result[0]?._id
                 });
-                return { message: translate("messages.applied"), success: true };
+                return {
+                    message: translate('messages.applied'),
+                    success: true
+                };
             } else {
                 return null;
             }
